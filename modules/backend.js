a78534bb2afb035aea4c86445d7d9a10cb4997a6
Steve Guidetti <stevotvr@gmail.com>
2020-08-17 12:59:21 -0700

    Add quotes backend

--- modules/backend.js
+++ modules/backend.js
@@ -121,6 +121,11 @@ class Backend {
           resolve({ tips: rows });
         });
       },
+      quotes: (resolve) => {
+        this.db.all('SELECT id, date, user, game, message FROM quotes ORDER BY id ASC', (err, rows) => {
+          resolve({ quotes: rows });
+        });
+      },
       raffle: (resolve) => {
         this.db.all('SELECT user FROM raffle ORDER BY user ASC', (err, rows) => {
           resolve({
@@ -516,6 +521,75 @@ class Backend {
           params.push(Date.now());
 
           this.db.run('INSERT INTO tips (user, message, date) VALUES (?, ?, ?)', params, () => {
+            if (!--count) {
+              resolve();
+            }
+          });
+        }
+      },
+      quotes: (resolve, req) => {
+        const filter = input => {
+          const params = [];
+          params.push(input.user.replace(/[^a-z\d_]/ig, '').toLowerCase());
+          params.push(input.game)
+          params.push(input.message);
+
+          return params;
+        };
+
+        let count = 0;
+        if (typeof req.body.delete === "object") {
+          count += Object.keys(req.body.delete).length;
+        }
+
+        if (Array.isArray(req.body.update)) {
+          count += req.body.update.length;
+        }
+
+        if (req.body.add) {
+          count++;
+        }
+
+        if (!count) {
+          resolve();
+        }
+
+        if (typeof req.body.delete === "object") {
+          const stmt = this.db.prepare('DELETE FROM quotes WHERE id = ?');
+
+          for (const key in req.body.delete) {
+            stmt.run(+key.substr(1), () => {
+              if (!--count) {
+                resolve();
+              }
+            });
+          }
+
+          stmt.finalize();
+        }
+
+        if (Array.isArray(req.body.update)) {
+          const stmt = this.db.prepare('UPDATE quotes SET user = ?, game = ?, message = ? WHERE id = ?');
+
+          req.body.update.forEach(row => {
+            const params = filter(row);
+            params.push(+row.id);
+
+            stmt.run(params, () => {
+              if (!--count) {
+                resolve();
+              }
+            });
+          });
+
+          stmt.finalize();
+        }
+
+        if (req.body.add) {
+          const params = filter(req.body);
+          params.push(Date.now());
+
+          this.db.run('INSERT INTO quotes (user, game, message, date) VALUES (?, ?, ?, ?)', params, () => {
             if (!--count) {
               resolve();
             }
