d9cc26b6247837a76626b5056acf705fd1dc2470
Steve Guidetti <stevotvr@gmail.com>
2020-08-30 16:31:49 -0700

    Add giveaway database for giving away game keys

--- modules/backend.js
+++ modules/backend.js
@@ -75,7 +75,7 @@ export default class Backend {
       })
       .then((redir = true) => {
         if (redir) {
-          res.redirect(req.params.page);
+          res.redirect('back');
         } else {
           res.end();
         }
@@ -144,6 +144,29 @@ export default class Backend {
             users: rows.map(v => v.user)
           });
         });
+      },
+      giveaway: (resolve, req, res) => {
+        if (req.query.group) {
+          this.db.get('SELECT id, name FROM giveaway_groups WHERE id = ?', req.query.group, (err, row) => {
+            if (!row) {
+              res.status(404);
+              resolve();
+
+              return;
+            }
+
+            this.db.all('SELECT id, name, key FROM giveaway WHERE groupId = ? ORDER BY name ASC', row.id, (err, rows) => {
+              resolve({
+                group: row,
+                items: rows
+              });
+            });
+          });
+        } else {
+          this.db.all('SELECT id, name, random FROM giveaway_groups ORDER BY name ASC', (err, rows) => {
+            resolve({ groups: rows });
+          });
+        }
       }
     };
   }
@@ -671,6 +694,144 @@ export default class Backend {
               resolve();
             }
           });
+        }
+      },
+      giveaway: (resolve, req) => {
+        if (req.body.group) {
+          const filter = input => {
+            const params = [];
+            params.push(input.name);
+            params.push(input.key);
+
+            return params;
+          };
+
+          let count = 0;
+          if (typeof req.body.delete === "object") {
+            count += Object.keys(req.body.delete).length;
+          }
+
+          if (Array.isArray(req.body.update)) {
+            count += req.body.update.length;
+          }
+
+          if (req.body.add) {
+            count++;
+          }
+
+          if (!count) {
+            resolve();
+          }
+
+          if (typeof req.body.delete === "object") {
+            const stmt = this.db.prepare('DELETE FROM giveaway WHERE id = ?');
+
+            for (const key in req.body.delete) {
+              stmt.run(+key.substr(1), () => {
+                if (!--count) {
+                  resolve();
+                }
+              });
+            }
+
+            stmt.finalize();
+          }
+
+          if (Array.isArray(req.body.update)) {
+            const stmt = this.db.prepare('UPDATE giveaway SET name = ?, key = ? WHERE id = ?');
+
+            req.body.update.forEach(row => {
+              const params = filter(row);
+              params.push(+row.id);
+
+              stmt.run(params, () => {
+                if (!--count) {
+                  resolve();
+                }
+              });
+            });
+
+            stmt.finalize();
+          }
+
+          if (req.body.add) {
+            const params = filter(req.body);
+            params.unshift(+req.body.group);
+
+            this.db.run('INSERT INTO giveaway (groupId, name, key) VALUES (?, ?, ?)', params, () => {
+              if (!--count) {
+                this.app.db.loadSchedule();
+                resolve();
+              }
+            });
+          }
+        } else {
+          const filter = input => {
+            const params = [];
+            params.push(input.name);
+            params.push(!!input.random);
+
+            return params;
+          };
+
+          let count = 0;
+          if (typeof req.body.delete === "object") {
+            count += Object.keys(req.body.delete).length;
+          }
+
+          if (Array.isArray(req.body.update)) {
+            count += req.body.update.length;
+          }
+
+          if (req.body.add) {
+            count++;
+          }
+
+          if (!count) {
+            resolve();
+          }
+
+          if (typeof req.body.delete === "object") {
+            const stmt1 = this.db.prepare('DELETE FROM giveaway WHERE groupId = ?');
+            const stmt2 = this.db.prepare('DELETE FROM giveaway_groups WHERE id = ?');
+
+            for (const key in req.body.delete) {
+              stmt1.run(+key.substr(1));
+              stmt2.run(+key.substr(1), () => {
+                if (!--count) {
+                  resolve();
+                }
+              });
+            }
+
+            stmt1.finalize();
+            stmt2.finalize();
+          }
+
+          if (Array.isArray(req.body.update)) {
+            const stmt = this.db.prepare('UPDATE giveaway_groups SET name = ?, random = ? WHERE id = ?');
+
+            req.body.update.forEach(row => {
+              const params = filter(row);
+              params.push(+row.id);
+
+              stmt.run(params, () => {
+                if (!--count) {
+                  resolve();
+                }
+              });
+            });
+
+            stmt.finalize();
+          }
+
+          if (req.body.add) {
+            this.db.run('INSERT INTO giveaway_groups (name, random) VALUES (?, ?)', filter(req.body), () => {
+              if (!--count) {
+                resolve();
+              }
+            });
+          }
         }
       },
       test: (resolve, req) => {
--- modules/database.js
+++ modules/database.js
@@ -107,6 +107,8 @@ export default class Database {
             db.run('CREATE TABLE IF NOT EXISTS raffle (user TEXT PRIMARY KEY, tickets INTEGER NOT NULL DEFAULT 1)');
             db.run('CREATE TABLE IF NOT EXISTS autoshoutout (user TEXT PRIMARY KEY)');
             db.run('CREATE TABLE IF NOT EXISTS quotes (id INTEGER PRIMARY KEY AUTOINCREMENT, date INTEGER NOT NULL, user TEXT NOT NULL, game TEXT NOT NULL, message TEXT NOT NULL)');
+            db.run('CREATE TABLE IF NOT EXISTS giveaway (id INTEGER PRIMARY KEY AUTOINCREMENT, groupId INTEGER NOT NULL, name TEXT NOT NULL, key TEXT NOT NULL)');
+            db.run('CREATE TABLE IF NOT EXISTS giveaway_groups (id INTEGER PRIMARY KEY AUTOINCREMENT, random INTEGER NOT NULL DEFAULT 0, name TEXT NOT NULL)');
           });
         });
       });
